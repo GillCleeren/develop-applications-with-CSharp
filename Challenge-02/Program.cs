001505865b5a274621bf42d1c36a1202c895c4ce
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-03-13 14:38:50 +0100

    Challenge-02-end added

---
+++ Challenge-02/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using CarvedRock.Backend;
+
+Product climbingShoes = new Product(1, "Climbing shoes", "A great shoe to climb the highest mountains", 102.15, 150, ProductStatus.InStock);
+
+Console.WriteLine(climbingShoes.DisplayProduct());
