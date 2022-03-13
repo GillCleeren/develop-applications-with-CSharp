199633bac37ebbac57b19748cf558a428af31c18
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-03-13 15:59:46 +0100

    Challenge-03-start added

---
+++ Challenge-03/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarvedRock.Backend
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double BasePrice { get; set; }
+        public int AmountInStock { get; set; }
+        public ProductStatus ProductStatus { get; set; }
+
+
+        public Product(int id, string name, string description, double basePrice, int amountInStock, ProductStatus productStatus)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            BasePrice = basePrice;
+            AmountInStock = amountInStock;
+            ProductStatus = productStatus;
+
+        }
+
+        public string DisplayProduct()
+        {
+            return $"Product ID: {Id}\nProduct name: {Name}\nDescription: {Description}\nBase Price: {string.Format(new System.Globalization.CultureInfo("en-US"), "{0:C2}", BasePrice)}\nAmount in stock: {AmountInStock}\nProduct status: {ProductStatus}\n";
+        }
+
+    }
+}
