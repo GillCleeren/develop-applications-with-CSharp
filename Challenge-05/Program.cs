f2db3fbb4f3e5cb780f68e56fe06a8c03276d586
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-03-13 17:34:38 +0100

    Challenge-05-start added

---
+++ Challenge-05/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using CarvedRock.Backend;
+
+//Footwear f1 = new Footwear(1, "Basic walking shoes", "Go where you want with these", 
+//    95.15, 87, ProductStatus.InStock, "047Y", "Black", "Leather", true);
+//ClimbingFootwear f2 = new ClimbingFootwear(2, "Go Up Climbers", "Ready to take on all mountains", 
+//    178.44, 23, ProductStatus.InStock, "77PL", "Green", "Textile", true, "Indoor climbing");
+//CyclingFootwear f3 = new CyclingFootwear(3, "CyclerPowX", "Get the best in class", 
+//    126.36, 54, ProductStatus.ComingSoon, "EEF6", "Black", "Leather", true, "All terrain");
+
+//Console.WriteLine(f1.DisplayProduct());
+//Console.WriteLine(f2.DisplayProduct());
+//Console.WriteLine(f3.DisplayProduct());
+
+//Footwear[] footwearProducts = new Footwear[] { f1, f2, f3 };
+
+//foreach (Footwear footwear in footwearProducts)
+//{
+//    Console.WriteLine(footwear.DisplayProduct());
+//}
+
+Console.ReadLine();
---
+++ Challenge-05/Utilities.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CarvedRock.Backend
+{
+    internal class Utilities
+    {
+        public static Product? ReadProductFromTextFile(string fileName)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, fileName);
+            string pattern = @"^[a-zA-Z0-9]+$";
+
+            if (File.Exists(path))
+            {
+                StreamReader sr = new StreamReader(path);
+                string line = sr.ReadLine();
+                sr.Close();
+                 
+                string[] parts = line.Split(';');
+                int id = int.Parse(parts[0]);
+                string name = parts[1];
+                string description = parts[2];
+                double basePrice = double.Parse(parts[3], new CultureInfo("en-US"));
+                int amountInStock = int.Parse(parts[4]);
+                ProductStatus status = Enum.Parse<ProductStatus>(parts[5]);
+                string productCode = parts[6];
+
+                Regex regex = new Regex(pattern);
+
+                if (!regex.IsMatch(productCode))
+                {
+                    productCode = string.Empty;
+                }
+
+                Product product = new(id, name, description, basePrice, amountInStock, status, productCode);
+
+                return product;
+            }
+            return null;
+        }
+    }
+}
