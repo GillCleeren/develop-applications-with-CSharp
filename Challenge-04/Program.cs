41b34d367eef3a3d55195dc619819d6c39661ab2
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-03-13 16:56:15 +0100

    Challenge-04-start added

---
+++ Challenge-04/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using CarvedRock.Backend;
+
+Product climbingShoes = Utilities.ReadProductFromTextFile("product1.txt");
+
+if (climbingShoes != null)
+{
+    Console.WriteLine(climbingShoes.DisplayProduct());
+}   
---
+++ Challenge-04/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CarvedRock.Backend
+{
+    internal class Utilities
+    {
+        public static Product? ReadProductFromTextFile(string fileName)
+        {
+            
+            return null;
+        }
+    }
+}
