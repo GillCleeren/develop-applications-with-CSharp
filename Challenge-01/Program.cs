ea219197fc2db12b7b98227e22a350b65ccf59f8
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-03-13 13:35:19 +0100

    Challenge-01-start added

---
+++ Challenge-01/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using CarvedRock.Backend;
+
+
+//string salePrice = Utilities.CalculateSalePrice(100, 60, true, 0.2, 0.1);
+
+//Console.WriteLine($"The sale price is {salePrice}");
+
+Console.ReadLine();
