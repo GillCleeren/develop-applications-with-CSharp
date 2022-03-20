611458901334f6ca6375be53f64df8a654dc493c
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-03-20 16:50:49 +0100

    Challenge-08-start added

---
+++ Challenge-08/CyclingFootwear.cs
@@ -0,0 +1,20 @@
+﻿namespace CarvedRock.Backend
+{
+    public class CyclingFootwear : Footwear
+    {
+        public string CyclingType { get; set; }
+
+        public CyclingFootwear(int id, string name, string description, double basePrice, int amountInStock, ProductStatus productStatus, string productCode, string color, string material, bool waterproof, string cyclingType) : base(id, name, description, basePrice, amountInStock, productStatus, productCode, color, material, waterproof)
+        {
+            CyclingType = cyclingType;
+        }
+
+        public override string DisplayProduct()
+        {
+            return $"CYCLING FOOTWEAR\n" +
+                   $"----------------\n" +
+                   $"Product ID: {Id}\nProduct name: {Name}\nDescription: {Description}\nBase Price: {string.Format(new System.Globalization.CultureInfo("en-US"), "{0:C2}", BasePrice)}\nSale Price:{string.Format(new System.Globalization.CultureInfo("en-US"), "{0:C2}", SalePrice)}\nAmount in stock: {AmountInStock}\nProduct status: {ProductStatus}\nProduct Code: {ProductCode}\n";
+
+        }
+    }
+}
---
+++ Challenge-08/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+using CarvedRock.Backend;
+
+List<Product> products = Utilities.ReadInventoryFile("inventory.txt");
+foreach (var product in products)
+{
+    Console.WriteLine(product.DisplayProduct());
+}
+
+
+var cyclingFootwearInStock = Utilities.GetFilteredCyclingFootwear(products);
+
+foreach (var product in cyclingFootwearInStock)
+{
+    Console.WriteLine(product.DisplayProduct());
+}
