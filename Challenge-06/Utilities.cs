ede79cf054947efca2d5f15a7ef2b31fdff5aaa1
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-03-20 15:38:34 +0100

    Challenge-06-start added

---
+++ Challenge-06/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CarvedRock.Backend
+{
+    public class Utilities
+    {
+        public static List<Product> ReadInventoryFile(string fileName)
+        {
+            return null;
+        }
+    }
+}
