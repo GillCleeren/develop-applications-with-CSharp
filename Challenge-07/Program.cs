45e56d606809a280737bbae6948b6912c92a4359
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-03-20 16:19:42 +0100

    Challenge-07-start added

---
+++ Challenge-07/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+using CarvedRock.Backend;
+
+List<Product> products = Utilities.ReadInventoryFile("inventory.txt");
+foreach (var product in products)
+{
+    Console.WriteLine(product.DisplayProduct());
+}
+
+
