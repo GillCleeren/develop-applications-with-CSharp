1dc307981b069cbd0dcc285745c036594f453091
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-03-20 19:51:31 +0100

    Challenge-10-end added

---
+++ Challenge-10/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace CarvedRock.Backend
+{
+    internal static class Extensions
+    {
+        public static string GetDescription(this Enum enumValue)
+        {
+            Type type = enumValue.GetType();
+            string enumerationName = Enum.GetName(type, enumValue);
+
+            if (enumerationName != null)
+            {
+                FieldInfo field = type.GetField(enumerationName);
+                if (field != null)
+                {
+                    DescriptionAttribute attr = Attribute.GetCustomAttribute(field,typeof(DescriptionAttribute)) as DescriptionAttribute;
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+            return enumValue.ToString();
+        }
+    }
+}
---
+++ Challenge-10/Footwear.cs
@@ -0,0 +1,24 @@
+﻿namespace CarvedRock.Backend
+{
+    public class Footwear : Product
+    {
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public bool Waterproof { get; set; }
+
+        public Footwear(int id, string name, string description, double basePrice, int amountInStock, ProductStatus productStatus, string productCode, string color, string material, bool waterproof) : base(id, name, description, basePrice, amountInStock, productStatus, productCode)
+        {
+            Color = color;
+            Material = material;
+            Waterproof = waterproof;
+        }
+
+        public override string DisplayProduct()
+        {
+            return $"FOOTWEAR\n" +
+                   $"--------\n" +
+                   $"Product ID: {Id}\nProduct name: {Name}\nDescription: {Description}\nBase Price: {string.Format(new System.Globalization.CultureInfo("en-US"), "{0:C2}", BasePrice)}\nSale Price:{string.Format(new System.Globalization.CultureInfo("en-US"), "{0:C2}", SalePrice)}\nAmount in stock: {AmountInStock}\nProduct status: {ProductStatus.GetDescription()}\nProduct Code: {ProductCode}\n";
+
+        }
+    }
+}
